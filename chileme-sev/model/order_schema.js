1afc4ceaeba050693b667dd05b25c68aa0ea3ad8
18336523297 <nan@18336523297>
2020-04-22 11:01:44 +0800

    后台项目

---
+++ chileme-sev/model/order_schema.js
@@ -0,0 +1,12 @@
+/**订单表 
+ * 字段                   描述                     类型       
+ *  orderNo           订单号                   String
+ *  orderType        订单状态                String   (已创建，进行中，已挂起，已完成)
+ *  detail               订单包含菜品          Arrray 
+ *  createTime      下单时间                 Number
+ *  way                 支付方式                 String
+ *  price                订单总金额             Number
+ *  payTime          支付时间                 Number
+ *  createPersonName   下单用户名     String
+ *  createPersonId   下单用户              Number 
+ * */ 
---
+++ chileme-sev/model/user_schema.js
@@ -0,0 +1,8 @@
+/**   用户表
+ * 字段	           描述          类型
+ *      username    用户名      String
+ *      password     密码         String
+ *      mobile        手机号       String
+ *      createDate  创建时间    Number (时间戳)
+ *      lastUpdate  上次更新时间   Number     
+*/ 
