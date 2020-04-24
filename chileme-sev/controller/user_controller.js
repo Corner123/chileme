abbc47e98a473e2bba16c7596812fa3d390eac0c
18336523297 <nan@18336523297>
2020-04-24 09:25:23 +0800

    定义登录注册接口

--- chileme-sev/controller/user_controller.js
+++ chileme-sev/controller/user_controller.js
@@ -1,17 +1,40 @@
-//引入model文件夹中定义好的Schema
-//构造中间件
+//1引入model文件夹中定义好的Schema
+const User=require('../model/user_schema')
+//2构造中间件
 
+/* async/await 
+    async 异步  放在function关键字之前，声明该函数为一个异步函数   返回一个promise对象
+    async function fn(){}
+    await 同步 必须和async搭配使用不能单独出现  等待上一个async函数完成之后再执行后面的代码
+    async function fn(){
+        await .......
+    }
+*/ 
 
 //注册
 const register=async ctx=>{
     let data=ctx.request.body
-    //1.验证数据是否合法   账号，密码，手机号（格式、手机号唯一性）
-    ctx.body='register！'
+    // 1.验证数据是否合法    账号，密码，手机号（格式/手机号唯一性）
+    if(!Util.regUsername.test(data.username))
+    ctx.body={
+        code:200,
+        flag:false,
+        type:'format error',
+        msg:'账号'
+    }
+}
+// 登录
+const login = async ctx => {
+    let data = ctx.request.body
+    // 1.验证登录信息是否合法  能否再库中查到该账号，账号信息和密码是否匹配
+    // 2.登录成功
+    ctx.body = 'login!'
 }
 
+//3暴露中间件
 
-//登录
-const login=async ctx=>{
-    let data=ctx.request.body
-    ctx.body='login!'
+module.exports = {
+    register,
+    login
 }
+
--- chileme-sev/router/router.js
+++ chileme-sev/router/router.js
@@ -1,14 +1,22 @@
-const Router=require('koa-router')
-const router=new Router()
-// 接口
-router.get('/',async ctx=>{
-    ctx.body='hello'
-})
-router.get('/test',async ctx=>{
-    ctx.body='test'
-})
-// 用户接口
+// 1.引入koa-router 及 controller中定义好的中间件
+const Router = require('koa-router') // 引入koa-router
+const User = require('../controller/user_controller')
+// 2.对koa-router 进行实例化
+const router = new Router()  // 对路由进行实例化
+// 3.定义接口
 
+// 接口
+// test
+router.get('/', async ctx => {
+    ctx.body = 'hello'
+})
+router.get('/test', async ctx => {
+    ctx.body = 'test'
+})
+
+// 用户接口
+router.post('/api/user/register', User.register)// 注册
+router.post('/api/user/login', User.login)// 登录
 
 // 商品接口
 
@@ -16,4 +24,5 @@ router.get('/test',async ctx=>{
 // 订单接口
 
 
-module.exports=router
+// 4.暴露接口
+module.exports = router
