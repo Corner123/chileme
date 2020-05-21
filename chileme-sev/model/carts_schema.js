8ff5d73631596a157c7dd1252feec2b54c3b7d3c
Corner123 <525855124@qq.com>
2020-05-21 20:31:14 +0800

    购物车

---
+++ chileme-sev/model/carts_schema.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+const schema=mongoose.Schema
+const Carts=new schema({
+    id:String,//当前商品在数据库的唯一标识
+    name:String,//商品名
+    Price:Number,//商品价格
+    count:Number,//商品数量
+})
+module.exports=mongoose.model('Carts',Carts)
